0d6f4fbf62410866a6eab0329d5918c65c741c4d
andreichernov <andreichernov@list.ru>
2016-05-21 19:32:53 +0300

    Initial commit

---
+++ src/main/java/ru/andreichernov/dao/UserDAO.java
@@ -0,0 +1,14 @@
+package ru.andreichernov.dao;
+
+import ru.andreichernov.entity.User;
+
+import java.util.List;
+
+public interface UserDAO {
+    public int createUser(User user);
+    public User updateUser(User user);
+    public void deleteUser(int id);
+    public List<User> getAllUsers();
+    public User getUser(int id);
+    public List<User> getAllUsers(String userName);
+}
---
+++ src/main/java/ru/andreichernov/service/UserService.java
@@ -0,0 +1,13 @@
+package ru.andreichernov.service;
+import ru.andreichernov.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    public int createUser(User user);
+    public User updateUser(User user);
+    public void deleteUser(int id);
+    public List<User> getAllUsers();
+    public User getUser(int id);
+    public List<User> getAllUsers(String userName);
+}
